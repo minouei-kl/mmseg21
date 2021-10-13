d911e77c4cf4cfc99337f4ff842ff567cbb10281
minouei <mohammad.minooee@gmail.com>
2021-10-13 14:31:23 +0200

    ver7

--- configs/0ver/segformer_mit-b2_512x512_160k_ver.py
+++ configs/0ver/segformer_mit-b2_512x512_160k_ver.py
@@ -32,22 +32,20 @@ model = dict(
     test_cfg=dict(mode='whole'))
 dataset_type = 'ADE20KDataset'
 data_root = '/netscratch/minouei/versicherung/version7'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (512, 512)
+
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', reduce_zero_label=True),
-    dict(type='Resize', img_scale=(2048, 512), ratio_range=(0.5, 2.0)),
-    dict(type='RandomCrop', crop_size=(512, 512), cat_max_ratio=0.75),
-    dict(type='RandomFlip', prob=0.5),
-    dict(type='PhotoMetricDistortion'),
+    dict(type='Resize', img_scale=(640, 512), ratio_range=None),
+    # dict(type='RandomCrop', crop_size=(512, 512), cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.0),
+    # dict(type='PhotoMetricDistortion'),
     dict(
         type='Normalize',
         mean=[123.675, 116.28, 103.53],
         std=[58.395, 57.12, 57.375],
         to_rgb=True),
-    dict(type='Pad', size=(512, 512), pad_val=0, seg_pad_val=255),
+    dict(type='Pad', size=(640, 512), pad_val=0, seg_pad_val=255),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg'])
 ]
@@ -55,7 +53,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(2048, 512),
+        img_scale=(640, 512),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
